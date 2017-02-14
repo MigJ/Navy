af290877c11e0b29a03c92db0e0c91c22f0a085f
Miguel Joubert <miguel.joubert@epitech.eu>
2017-02-14 17:11:37 +0100

    win

--- src/game.c
+++ src/game.c
@@ -5,7 +5,7 @@
 ** Login   <miguel.joubert@epitech.net>
 ** 
 ** Started on  Mon Jan 30 15:22:18 2017 miguel joubert
-** Last update Tue Feb 14 16:42:13 2017 miguel joubert
+** Last update Tue Feb 14 16:45:48 2017 miguel joubert
 */
 
 #include "../include/my.h"
@@ -90,7 +90,6 @@ int	host(t_elem E, t_map M)
       free(E.s);
     }
   (E.win == 1) ?  my_putstr("\nI won\n", 1) : my_putstr("\nEnemy won\n", 1);
-  usleep(1000);
   return ((E.loose == 1) ? 1 : 0);
 }
 
@@ -122,7 +121,6 @@ int	client(t_elem E, t_map M)
       free(E.s);
     }
   (E.loose == 1) ? my_putstr("\nEnemy won\n", 1) : my_putstr("\nI won\n", 1);
-  usleep(1000);
   return ((E.loose == 1) ? 1 : 0);
 }
 
